f1de9f3adcb0cb7e786a4586247b39ef8f16958c
Hagliberto <hagliberto@gmail.com>
2023-10-17 22:03:47 -0300

    Criação dos usuários

---
+++ inc/usuarios.php
@@ -0,0 +1,18 @@
+<?php
+
+// usuarios
+
+return[
+    [
+        'usuario' => 'uauraio1@gmail.com',
+        'senha' => password_hash('senha001', PASSWORD_DEFAULT)
+    ],
+    [
+        'usuario' => 'uauraio2@gmail.com',
+        'senha' => password_hash('senha002', PASSWORD_DEFAULT)
+    ],
+    [
+        'usuario' => 'uauraio3@gmail.com',
+        'senha' => password_hash('senha003', PASSWORD_DEFAULT)
+    ]
+]
